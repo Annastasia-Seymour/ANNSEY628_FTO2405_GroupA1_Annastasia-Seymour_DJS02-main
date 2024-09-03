d8d6e1a803e9571f25bccc405289af9d33def247
Annastasia-Seymour <stasia1seymour@gmail.com>
2024-09-03 11:33:03 +0200

    Adds an alert when there empty fields

--- scripts.js
+++ scripts.js
@@ -1,9 +1,28 @@
 const form = document.querySelector("[data-form]");
 const result = document.querySelector("[data-result]");
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const entries = new FormData(event.target);
-  const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = dividend / divider;
-});
+const inputDivider = document.getElementById("divider").value;
+const inputDividend = document.getElementById("dividend").value;
+
+
+if ((inputDividend.innerText = 'null') && (inputDivider.innerText ="null")){
+  alert("Fields cannot be empty");
+console.warn("Fields cannot be empty");
+
+} else {
+  submitButton();
+}
+
+function submitButton (){
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const entries = new FormData(event.target);
+    const { dividend, divider } = Object.fromEntries(entries);
+    result.innerText = dividend / divider;
+  });
+  
+}
+
+
+
+//let calculationResult = document.getElementById("calculationResult");
