39b7c724b25798a94055d04716e63d49a6e12178
Annastasia-Seymour <stasia1seymour@gmail.com>
2024-09-05 11:16:49 +0200

    Successfully crashed program

--- scripts.js
+++ scripts.js
@@ -1,8 +1,17 @@
 const form = document.querySelector("[data-form]");
 const result = document.querySelector("[data-result]");
-//const regex = /^pet_\d{4}[a-zA-Z]+$/
+const regex = /[a-zA-Z]+$/
 
-function submitButton (){
+
+function isString(inputSearch){
+  if (regex.test(inputSearch)) {
+    console.warn("Inputs should be numbers, not strings");
+    return true;
+  } else {
+    return false;
+  }
+ 
+}
   form.addEventListener("submit", (event) => {
     event.preventDefault();
     const entries = new FormData(event.target);
@@ -13,34 +22,43 @@ try{
     result.innerText ="Division not performed. Both values are required in inputs. Try again";
     console.warn("Fields cannot be empty");
     form.reset();  // Clear the inputs
-    
-  } else if(Number(divider === 0)) {
-    result.innerText ="Division not performed. Invalid number provided. Try again";
+    return;
+  } 
+  if (Number(divider) === 0 || Number(dividend === 0)) {
+    result.innerText ="Division not performed. Invalid number provided. Try again ";
     console.error(`${dividend} cannot be divided by ${divider}, results in undefined`);
     form.reset();  
+    return;
   //no need for what i was think submitButton() used form.reset instead;
   }
-    else if (isNaN(Number(divider)) || (isNaN(Number(dividend)))) {
-    result.innerText ="Please enter a numeric value";
-    form.reset(); 
+  if (isString(divider) || (isString(dividend))) {
+   // result.innerText = "Division not performed. Invalid input. Only numbers are allowed.";
+    //console.warn("Inputs should be numbers, not strings");
+   throw Error("Invalid input detected: Non-numeric values entered");
+    
    
   }
-    else {
-    result.innerText = Math.floor(dividend / divider);
-    console.log(dividend / divider);
-    
-  }
-}
+  
+  const resultQuotient = Math.floor(dividend / divider);
+    result.innerText = resultQuotient;
+    console.log(dividend / divider);//outputs in the console includes remainder
+    form.reset();
+    return;
+  
+ }
 
 catch (error) {
-  console.error(error);  // Log the error with the call stack i think
-  result.innerText = "An error occurred. Please try again.";
+  //console.error(error);  // Log the error with the call stack i think
+  //result.innerText = "An error occurred. Please try again.";
+ // const originalForm = document.getElementById('Form');
+ // Replace the entire screen content with a critical error message
+ document.body.innerHTML = `<h1>Something critical went wrong. Please reload the page.</h1>`;
+    
+ // Log the error and call stack to the console
+ console.error(error.stack);  // Logs the error along with the call stack
 }
 });
   
-}
-
-
-submitButton();
+//}submitButton();
 //remember to remove function? 
 
